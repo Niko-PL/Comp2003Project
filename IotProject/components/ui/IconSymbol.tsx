132894e5925a38f396f9bc0fea63cecaf9ec86d2
Tom Cogzell <tomcogzell8@gmail.com>
2025-01-17 11:50:50 +0000

    Fixed icon bugs not working on web and possibily android

--- IotProject/components/ui/IconSymbol.tsx
+++ IotProject/components/ui/IconSymbol.tsx
@@ -11,6 +11,9 @@ const MAPPING = {
   // See SF Symbols in the SF Symbols app on Mac.
   'house.fill': 'home',
   'paperplane.fill': 'send',
+  'plus': 'add',
+  'qrcode': 'qr-code',
+  'gear': 'settings',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
 } as Partial<
